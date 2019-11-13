ed35baaa2f231371b21c82f6024331ae6cf9cbb5
thibaultmollier <thibault.mollier@lilo.org>
2019-11-13 18:22:12 +0100

    V1

--- main.c
+++ main.c
@@ -3,11 +3,22 @@
 
 int main(){
     FILE *pfile;
-    char buffer[20];
+    char buffer;
+
+    puts("Compression en codage de Huffman");
+    puts("--------------------------------");
 
     pfile=fopen("file.txt","r");
-    fread(buffer,sizeof(char)*20,1,pfile);
-    printf("%s\n",buffer);
+    if (pfile)
+    {
+        while (buffer!=EOF)
+        {
+            buffer=fgetc(pfile);
+            printf("%c",buffer);
+        }
+        
+    }
+    
     fclose(pfile);
 
 }
